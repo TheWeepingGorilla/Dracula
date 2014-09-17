99eae2a612c9b562a6042713dd9b92c78fa50922
Sean Morrison <student@epicodus.com>
2014-09-17 15:15:55 -0700

    Add application.hbs for navbar.

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 Blogger.Router.map(function() {
-  this.resource('posts');
-});
-
-Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
   this.resource('posts');
   this.resource('about');
-  this.resource('contact');
+  this.resource('contact', function() {
+    this.resource('phone');
+    this.resource('email');
+  });
   this.resource('more');
 });
+
+
