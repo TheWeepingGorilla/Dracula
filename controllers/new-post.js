3d0582b5f46bc0641593187577f580d587593e65
Sean Morrison <student@epicodus.com>
2014-09-18 15:21:03 -0700

    Add comments to posts directly.  Uses DS relationships.

--- controllers/new-post.js
+++ controllers/new-post.js
@@ -10,3 +10,6 @@ Blogger.NewPostController = Ember.Controller.extend({
     }
   }
 });
+
+
+
--- models/post.js
+++ models/post.js
@@ -1,4 +1,7 @@
 Blogger.Post = DS.Model.extend({
   title: DS.attr(),
-  body: DS.attr()
+  body: DS.attr(),
+  comments: DS.hasMany('comment', {async: true})
 });
+
+
