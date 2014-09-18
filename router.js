d4ef489fbb90401f9ac3a60e188d3b5eed9a3129
Sean Morrison <student@epicodus.com>
2014-09-18 11:30:09 -0700

    List out comments.

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
+  this.resource('recent-comments');
   this.resource('posts');
   this.resource('about');
   this.resource('contact', function() {
