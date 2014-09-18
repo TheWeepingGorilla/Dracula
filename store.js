74ae461531ba36ec0b43192c57ff4c0a8761a30d
Sean Morrison <student@epicodus.com>
2014-09-18 12:12:06 -0700

    Sort posts by title

--- store.js
+++ store.js
@@ -11,7 +11,7 @@ var posts = [
   },
   {
     id: '3',
-    title: "Thievery",
+    title: "A Thievery",
     body: "Every scrap of paper was gone, and with it all my notes, my memoranda, relating to railways and travel, my letter of credit, in fact all that might be useful to me were I once outside the castle. I sat and pondered awhile, and then some thought occurred to me, and I made search of my portmanteau and in the wardrobe where I had placed my clothes."
   }
 ];
